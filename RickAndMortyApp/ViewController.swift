2eded6bf5b72d9dc600f56801d1be165942982a8
Liimmaa <halimammegwa@gmail.com>
2023-06-20 03:55:29 +0100

    Initial Commit

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Chioma Amanda Mmegwa  on 20/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
